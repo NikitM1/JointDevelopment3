68d86451855b9f1a4af818574e8f4a2e992642e0
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-02-24 23:14:45 +0300

    Parser completed Added params -F, -N, -E, message1, message2

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -4,25 +4,48 @@ import sys
 
 
 parser = argparse.ArgumentParser(
-    prog=os.path.basename(sys.argv[0]),
-    description="Generates an ASCII image of a cow saying the given text",
+    description="Generates an image of two cows saying the given text"
 )
 
 parser.add_argument(
     "-e",
     type=str,
-    help="An eye string. This is ignored if a preset mode is given",
-    dest="eyes",
-    default=Option.eyes,
-    metavar="eye_string",
+    help="First cow's eyes string. This is ignored if a preset mode is given",
+    default='oo'
 )
 parser.add_argument(
-    "-f", type=str, metavar="cowfile",
-    help="Either the name of a cow specified in the COWPATH, "
-         "or a path to a cowfile (if provided as a path, the path must "
-         "contain at least one path separator)",
+    "-f", 
+    type=str, 
+    help="Animal for the first 'cow'"
 )
 parser.add_argument(
-    "-n", action="store_false",
-    help="If given, text in the speech bubble will not be wrapped"
+    "-n",
+    help="First cow's speech wrapped"
 )
+parser.add_argument(
+    "-E",
+    type=str,
+    help="Second cow's eyes string. This is ignored if a preset mode is given",
+    default='oo'
+)
+parser.add_argument(
+    "-F", 
+    type=str, 
+    help="Animal for the second 'cow'"
+)
+parser.add_argument(
+    "-N",
+    help="First cow's speech wrapped"
+)
+parser.add_argument(
+    "message1", 
+    default=None, 
+    help="First cow's speech"
+)
+parser.add_argument(
+    "message2", 
+    default=None, 
+    help="Second cow's speech"
+)
+
+args = parser.parse_args()
