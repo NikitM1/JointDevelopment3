c3d3b08404484a31908026574612b2ac06f9ee1b
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-03-01 02:04:51 +0300

    All functions are done Added function ask entering a guess; added function inform that prints user's score. Made a test launch

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -6,17 +6,27 @@ def bullscows(guess: str, solution: str) -> tuple[int, int]:
     return (bulls, cows)
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
-    solution=words[random.random]
-    asksCount=0
+    solution=words[random.randint(0, len(words))]
+    askCount=0
     b=-1
     while b!=len(solution):
         guess=ask("Enter the word: ", words)
         b,c=bullscows(guess, solution)
         inform("Bulls: {}, Cows: {}", b, c)
         askCount+=1
-    #print("You found the solution in", askCount, "attempts. Well done!")
+    print("You found the solution in", askCount, "attempts. Well done!")
     return askCount
 
-def ask(*args): pass
+def ask(prompt: str, valid: list[str] = None) -> str:
+    print(prompt, end='')
+    guess=input()
+    if valid==None: return guess
+    while guess not in valid:
+        print('You have to enter a valid word\n'+prompt, end='')
+        guess=input()
+    return guess
 
-def inform(*args): pass
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+    
+#gameplay(ask, inform, ['песни', 'пляски'])
