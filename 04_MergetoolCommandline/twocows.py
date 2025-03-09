59446cab6ab58f6fa29120ee85181b3647f52487
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-03-09 18:16:01 +0300

    Added cowthink and cowsay User can now run cowthink and cowsay, but without reply feature

--- 04_MergetoolCommandline/twocows.py
+++ 04_MergetoolCommandline/twocows.py
@@ -2,6 +2,16 @@ import cmd
 import shlex
 from cowsay import cowsay,cowthink,list_cows,make_bubble
 
+def parseArguments(args):
+    words=shlex.split(args)
+    message=words[0]
+    kwargs={}
+    if len(words)>=2: kwargs['cow']=words[1]
+    for arg in words[2:]:
+        key,value=arg.split('=', 1)
+        kwargs[key]=value
+    return message, kwargs
+
 class CowsayShell(cmd.Cmd):
     intro='Welcome to the twocows shell!\nType help or ? for a list of commands\n'
     prompt='twocows> '
@@ -16,6 +26,24 @@ class CowsayShell(cmd.Cmd):
     def do_make_bubble(self, text):
         """usage: make_bubble [<text>]\n\nCreate a speech bubble for text"""
         print(make_bubble(text))
+        
+    def do_cowsay(self, args):
+        """usage: cowsay <request-message> [<cow-name> {<parameter-name>=<value>}] reply <answer-message> [<cow-name> {<parameter-name>=<value>}]\n\nCreate an image of a talking cow"""
+        try:
+            message,kwargs=self.parseArguments(args)
+            cowthink_message=cowsay(message, **kwargs)
+            print(cowthink_message)
+        except Exception as e:
+            print('An error occurred while executing:',e)
+    
+    def do_cowthink(self, args):
+        """usage: cowsay <request-message> [<cow-name> {<parameter-name>=<value>}] reply <answer-message> [<cow-name> {<parameter-name>=<value>}]\n\nCreate an image of a thinking cow"""
+        try:
+            message,kwargs=parseArguments(args)
+            cowthink_message=cowthink(message, **kwargs)
+            print(cowthink_message)
+        except Exception as e:
+            print('An error occurred while executing:',e)    
     
     def do_EOF(self, args):
         return True
