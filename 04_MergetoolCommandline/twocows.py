865c68c7a62656e07c31e77933debe49914d7600
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-03-09 18:35:33 +0300

    Сhanged handling of function calls Different functions will be used to separate calls for 1 and 2 cows

--- 04_MergetoolCommandline/twocows.py
+++ 04_MergetoolCommandline/twocows.py
@@ -2,20 +2,36 @@ import cmd
 import shlex
 from cowsay import cowsay,cowthink,list_cows,make_bubble
 
-def parseArguments(args):
-    words=shlex.split(args)
-    message=words[0]
-    kwargs={}
-    if len(words)>=2: kwargs['cow']=words[1]
-    for arg in words[2:]:
-        key,value=arg.split('=', 1)
-        kwargs[key]=value
-    return message, kwargs
-
 class CowsayShell(cmd.Cmd):
     intro='Welcome to the twocows shell!\nType help or ? for a list of commands\n'
     prompt='twocows> '
-
+    
+    def parseArguments(self, words):
+        if len(words)==0: raise ValueError('cow-name is a required argument')
+        message=words[0]
+        kwargs={}
+        if len(words)>=2: kwargs['cow']=words[1]
+        for arg in words[2:]:
+            key,value=arg.split('=', 1)
+            kwargs[key]=value
+        return message, kwargs
+    
+    def alignMessages(self, pictures):
+        lines1, lines2=pictures[0].split('\n'), pictures[1].split('\n')
+        len1, len2=map(len, [lines1, lines2])
+        maxlen1, maxlen2=max(map(len,lines1)), max(map(len,lines2))
+        
+        lines1, lines2=[' '*maxlen1]*(len2-len1)+lines1, [' '*maxlen2]*(len1-len2)+lines2
+        return '\n'.join([lines1[i]+' '*(maxlen1-len(lines1[i]))+lines2[i] for i in range(len1)])
+        #for i in range(len1): print(lines1[i]+' '*(maxlen1-len(lines1[i]))+lines2[i])    
+    
+    def oneCowProcessing(self, f, words):
+        message, kwargs=self.parseArguments(words)
+        return f(message,**kwargs)
+    
+    def twoCowsProcessing(self, f, words):
+        pass
+    
     def do_list_cows(self,args):
         """usage: list_cows\n\nList of all available animals"""
         animals=list_cows()
@@ -30,20 +46,18 @@ class CowsayShell(cmd.Cmd):
     def do_cowsay(self, args):
         """usage: cowsay <request-message> [<cow-name> {<parameter-name>=<value>}] reply <answer-message> [<cow-name> {<parameter-name>=<value>}]\n\nCreate an image of a talking cow"""
         try:
-            message,kwargs=self.parseArguments(args)
-            cowthink_message=cowsay(message, **kwargs)
-            print(cowthink_message)
+            words=shlex.split(args)
+            print(self.oneCowProcessing(cowsay,words))
         except Exception as e:
             print('An error occurred while executing:',e)
     
     def do_cowthink(self, args):
         """usage: cowsay <request-message> [<cow-name> {<parameter-name>=<value>}] reply <answer-message> [<cow-name> {<parameter-name>=<value>}]\n\nCreate an image of a thinking cow"""
         try:
-            message,kwargs=parseArguments(args)
-            cowthink_message=cowthink(message, **kwargs)
-            print(cowthink_message)
+            words=shlex.split(args)
+            print(self.oneCowProcessing(cowthink,words))
         except Exception as e:
-            print('An error occurred while executing:',e)    
+            print('An error occurred while executing:',e)   
     
     def do_EOF(self, args):
         return True
